dd521c20324264e3eff781ec4b12afe2205203f0
Caio Antunes <caioefg@gmail.com>
2023-10-23 18:24:12 -0300

    feat: add Categories Category-item componentes home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,16 @@
-'use client'
+
 import { useSession } from "next-auth/react"
 import Image from "next/image"
+import Categories from "./componentes/categories";
 export default function Home() {
   return (
     <div className="p-5">
       <Image src="/banner-home01.png" alt="Até 55% de desconto" width={0} height={0} className="h-auto w-full" sizes="100vw" />
+
+      <div className="mt-8">
+        <Categories />
+
+      </div>
     </div>
   );
 }
