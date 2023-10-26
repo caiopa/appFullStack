5510ff00325d3b1037f55b823aa1e96296e28e23
Caio Antunes <caioefg@gmail.com>
2023-10-26 10:12:18 -0300

    fix: total subtotal on cart

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -42,14 +42,14 @@ export const CartProvider = ({children}: {children: React.ReactNode}) => {
   // total sem descontos
   const subTotal = useMemo(() => {
     return products.reduce((acc, product) => {
-      return acc + Number(product.basePrice)
+      return acc + Number(product.basePrice) * product.quantity
     }, 0)
   }, [products])
 
   // total com descontos
   const total = useMemo(() => {
     return products.reduce((acc, product) => {
-      return acc + Number(product.totalPrice)
+      return acc + Number(product.totalPrice) * product.quantity
     }, 0)
   }, [products])
   const addProductToCart = (product: CartProduct) => {
