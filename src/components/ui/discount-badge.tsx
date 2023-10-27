ea394e9582612e657ca21f9b52464eef3240618a
Caio Antunes <caioefg@gmail.com>
2023-10-27 15:41:57 -0300

    feat: add badge accordion and descount badge components

--- src/components/ui/discount-badge.tsx
+++ src/components/ui/discount-badge.tsx
@@ -1,5 +1,5 @@
 import { ArrowDownIcon } from "lucide-react";
-import { Badge, BadgeProps } from "./badge";
+import { Badge, BadgeProps } from "../../../badge";
 import { twMerge } from "tailwind-merge";
 
 const DiscountBadge = ({ children, className, ...props }: BadgeProps) => {
